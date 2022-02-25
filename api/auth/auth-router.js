5c6cf2bde46cfb75dfdf334a5feeb370cf798f3a
Dylan Baker <dylanmbaker@yahoo.com>
2022-02-24 20:11:23 -0600

    finished auth model and registration router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,32 +1,24 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const User = require('./auth-model');
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+router.post('/register', async (req, res, next) => {
+ const { username, password } = req.body;
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
-
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+ if (!username || !password || req.body === null) {
+  next({ message: 'username and password required' })
+ } else if (username) {
+    const checkUsername = await User.findBy({ username: username }).first();
+    if (checkUsername) {
+      next({ message: 'username taken' })
+    } else {
+     const passwordHash = bcrypt.hashSync(password, 8);
+      User.addUser({ username, password: passwordHash })
+       .then(added => {
+         res.status(200).json(added)
+       }).catch(next)
+    }
+ }
 });
 
 router.post('/login', (req, res) => {
