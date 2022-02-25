926f5f138abb1a63e3826229776a0cc669780888
Dylan Baker <dylanmbaker@yahoo.com>
2022-02-24 20:45:57 -0600

    finished restricted middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -17,37 +17,12 @@ router.post('/register', async (req, res, next) => {
             User.addUser({ username, password: passwordHash })
                 .then((added) => {
                     res.status(200).json(added);
-                })
-                .catch(next);
+                }).catch(next);
         }
     }
 });
 
 router.post('/login', async (req, res, next) => {
-    // res.end('implement login, please!');
-    /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
     const { username, password } = req.body;
 
     if (!username || !password || req.body === null) {
@@ -72,7 +47,7 @@ const generateToken = (user) => {
     username: user.username
   };
 
-  return jwt.sign(payload, 'secret message', { expiresIn: '1d' })
+  return jwt.sign(payload, 'secret', { expiresIn: '1d' })
 };
 
 module.exports = router;
